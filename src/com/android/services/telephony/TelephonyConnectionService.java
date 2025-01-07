ffff96158f85f654ab42f0802636a29aa8e25a44
Akshay Sundaram <quic_akshsund@quicinc.com>
2025-01-07 14:33:31 -0800

    Revert "Fix the hold button is not working in CS conference call"
    This reverts commit 4ac2fab2d2f100993b35900f00e83973b59894c8.
    
    Change-Id: I06420169fe4f281180c37eadb4f6e72863dc195e
    CRs-Fixed: 4027817

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -5644,11 +5644,9 @@ public class TelephonyConnectionService extends ConnectionService {
     // Helper function to check if there are multiple held calls
     private boolean hasMultipleHeldCalls() {
         int numHeldCalls = 0;
-        // Individual telephony connections show up as HELD when CS conference call is held
         for (Connection current : getAllConnections()) {
             if (isTelephonyConnection(current) &&
-                    (current.getState() == Connection.STATE_HOLDING) &&
-                    (current.getConference() == null)) {
+                    (current.getState() == Connection.STATE_HOLDING)) {
                 numHeldCalls++;
             }
         }
