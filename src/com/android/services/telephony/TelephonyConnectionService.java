cfae9c7eadb42915adeb7616f5ed0473b2347a6f
Akshay Sundaram <quic_akshsund@quicinc.com>
2025-01-07 15:35:55 -0800

    Revert "DSDA: Use new API isDsdsMode and isDsdsTransitionSupported"
    This reverts commit b0b96dfe443ebe73534ece308141780ac7b1ef21.
    
    Change-Id: Ie69b70bf82d08c20e2a057a1b9b179cd889949ae
    CRs-Fixed: 4027817

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -387,18 +387,6 @@ public class TelephonyConnectionService extends ConnectionService {
          * configuration. Returns 1 for DSDS, 2 for DSDA.
          */
         int getMaxNumberOfSimultaneouslyActiveSims();
-
-        /**
-         * Returns true if Multi SIM voice cpability is DSDS.
-         * Returns false for other cases.
-         */
-        boolean isDsdsMode();
-
-        /**
-         *  Returns true if on multisim devices, DSDA features are supported in non-DSDA modes
-         *  Returns false otherwise.
-         */
-        boolean isDsdsTransitionSupported();
     }
 
     private TelephonyManagerProxy mTelephonyManagerProxy;
@@ -448,24 +436,6 @@ public class TelephonyConnectionService extends ConnectionService {
             try {
                 return mTelephonyManager.isConcurrentCallsPossible()
                     || mTelephonyManager.getPhoneCapability().getMaxActiveVoiceSubscriptions() > 1;
-            } catch (IllegalStateException ise) {
-                return false;
-            }
-        }
-
-        @Override
-        public boolean isDsdsMode() {
-            try {
-                return mTelephonyManager.isDsdsMode();
-            } catch (IllegalStateException ise) {
-                return false;
-            }
-        }
-
-        @Override
-        public boolean isDsdsTransitionSupported() {
-            try {
-                return mTelephonyManager.isDsdsTransitionSupported();
             } catch (IllegalStateException ise) {
                 return false;
             }
