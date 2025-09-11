cab5cc23075484b1599526c5e8b56e5ebe5cd247
Aleksander Morgado <aleksandermj@google.com>
2025-09-11 14:35:43 +0000

    Use common helper from TelephonyUtils to enforce features
    The logic to decide when the feature enforcement check should trigger
    an exception has been moved to a common helper in TelephonyUtils.
    
    TELEPHONY_FEATURE_ENFORCEMENT_VENDOR_API_LEVEL is now also used in
    unit tests to refer to the vendor API level where the feature
    enforcement checks were enabled.
    
    Bug: 434622492
    Test: atest TeleServiceTests
    Flag: EXEMPT PURE_REFACTOR
    
    Change-Id: I4ad19c11a71ebb0a02931b5817e868ded6f95180

--- tests/src/com/android/phone/CarrierConfigLoaderTest.java
+++ tests/src/com/android/phone/CarrierConfigLoaderTest.java
@@ -17,6 +17,7 @@
 package com.android.phone;
 
 import static com.android.TestContext.STUB_PERMISSION_ENABLE_ALL;
+import static com.android.internal.telephony.util.TelephonyUtils.TELEPHONY_FEATURE_ENFORCEMENT_VENDOR_API_LEVEL;
 
 import static com.google.common.truth.Truth.assertThat;
 
@@ -436,10 +437,9 @@ public class CarrierConfigLoaderTest extends TelephonyTestBase {
                 eq(android.Manifest.permission.READ_PRIVILEGED_PHONE_STATE),
                 anyInt(), anyInt(), anyString());
 
-        // Replace field to set SDK version of vendor partition to Android V
-        int vendorApiLevel = Build.VERSION_CODES.VANILLA_ICE_CREAM;
+        // Replace field to set vendor API level to the one where the exceptions are enabled.
         replaceInstance(CarrierConfigLoader.class, "mVendorApiLevel", mCarrierConfigLoader,
-                vendorApiLevel);
+                TELEPHONY_FEATURE_ENFORCEMENT_VENDOR_API_LEVEL);
 
         doReturn(false).when(mPackageManager).hasSystemFeature(
                 eq(PackageManager.FEATURE_TELEPHONY_SUBSCRIPTION));
