fd3ba92fce11430d5ffc9b255d439d9e21cb0ec9
Pranav Madapurmath <pmadapurmath@google.com>
2025-10-21 14:02:41 -0700

    TelecomAccountRegistry async broadcast logic
    Move the broadcast logic in TelecomAccountRegistry to be executed async.
    We have seen several ANR reports in
    TelecomAccountRegistry#setupOnBootInternal on watch devices. This seems
    to be due to the expensives operations we perform by tearing down and
    re-registering phone accounts on the main thread when we receive
    broadcasts. These should be done on a separate handler thread instead.
    
    Bug: 451460260
    Flag: EXEMPT BUGFIX
    Test: atest TelecomAccountRegistryTest
    Test: atest TelephonyConnectionServiceTest
    Change-Id: I14e07bff39f2a3e68fbca61666b8cabafcc1d3fd

--- tests/src/com/android/services/telephony/TelecomAccountRegistryTest.java
+++ tests/src/com/android/services/telephony/TelecomAccountRegistryTest.java
@@ -20,7 +20,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -67,6 +67,7 @@ public class TelecomAccountRegistryTest extends TelephonyTestBase {
 
     private static final String TAG = "TelecomAccountRegistryTest";
     private static final int TEST_SUB_ID = 1;
+    private static final long TIMEOUT = 10000;
 
     // We need more functions that what TelephonyTestBase.mContext supports.
     // Use a local mocked Context to make life easier.
@@ -236,7 +237,7 @@ public class TelecomAccountRegistryTest extends TelephonyTestBase {
     private PhoneAccount verifyAndCaptureRegisteredPhoneAccount() {
         ArgumentCaptor<PhoneAccount> phoneAccountArgumentCaptor =
                 ArgumentCaptor.forClass(PhoneAccount.class);
-        verify(mTelecomManager, atLeastOnce()).registerPhoneAccount(
+        verify(mTelecomManager, timeout(TIMEOUT).atLeastOnce()).registerPhoneAccount(
                 phoneAccountArgumentCaptor.capture());
         return phoneAccountArgumentCaptor.getValue();
     }
@@ -255,11 +256,7 @@ public class TelecomAccountRegistryTest extends TelephonyTestBase {
         intent.putExtra(SubscriptionManager.EXTRA_SUBSCRIPTION_INDEX, subId);
         mUserSwitchedAndConfigChangedReceiver.onReceive(mMockedContext, intent);
         mTestableLooper.processAllMessages();
-    }
 
-    private void onSubscriptionsChanged() {
-        Log.d(TAG, "Change subscriptions...");
-        mOnSubscriptionsChangedListener.onSubscriptionsChanged();
     }
 
     private void onAddSubscriptionListenerFailed() {
@@ -287,6 +284,7 @@ public class TelecomAccountRegistryTest extends TelephonyTestBase {
         Log.d(TAG, "Broadcast ACTION_LOCALE_CHANGED...");
         Intent intent = new Intent(Intent.ACTION_LOCALE_CHANGED);
         mLocaleChangedBroadcastReceiver.onReceive(mMockedContext, intent);
+        mTestableLooper.processAllMessages();
     }
 
     private void onNetworkCountryChanged() {
