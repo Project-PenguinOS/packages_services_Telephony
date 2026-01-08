e15473b3b7c5a6fceb96c0887ef26439a2017bf8
Hyein Yu <hyeinyu@google.com>
2026-01-08 02:16:17 +0000

    TelephonyShellCommand for SubscriptionManager#getPhoneNumber with shell uid
    Bug: 474143192
    Test: cts SubscriptionManagerTest
    Flag: com.android.internal.telephony.flags.get_phone_number_ts43_api
    Change-Id: I7d698a7d9ad3444dc278c22a0fb095a34f5e2611

--- src/com/android/phone/settings/hiddenmenu/PhoneInformationV2FragmentDeviceDetails.java
+++ src/com/android/phone/settings/hiddenmenu/PhoneInformationV2FragmentDeviceDetails.java
@@ -42,7 +42,6 @@ import androidx.fragment.app.Fragment;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.PhoneFactory;
 import com.android.internal.telephony.euicc.EuiccConnector;
-import com.android.internal.telephony.flags.Flags;
 import com.android.phone.R;
 
 import java.util.Locale;
@@ -344,10 +343,6 @@ public class PhoneInformationV2FragmentDeviceDetails extends Fragment {
                         + subMgr.getPhoneNumber(subId, SubscriptionManager.PHONE_NUMBER_SOURCE_UICC)
                         + ", IMS:"
                         + subMgr.getPhoneNumber(subId, SubscriptionManager.PHONE_NUMBER_SOURCE_IMS);
-        if (Flags.getPhoneNumberTs43Api()) {
-            number += ", TS43:" + subMgr.getPhoneNumber(
-                    subId, SubscriptionManager.PHONE_NUMBER_SOURCE_TS43);
-        }
         number += " }";
         mLine1Number.setText(number);
     }
