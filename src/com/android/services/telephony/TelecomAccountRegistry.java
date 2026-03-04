74d9e081dd9f8f96683e7a375a8888dfbcd1878d
Alvin Dey <alvidey@qti.qualcomm.com>
2026-03-04 12:53:49 +0530

    Revert "IMS : Move RTT downgrade and upgrade logic completely to AOSP."
    This reverts commit 178623e2a00a24157559be706a6ca347f3a9d8fc.
    
    Change-Id: I1c2ea21609598171f14a622705459bfd47b24f67
    CRs-Fixed: 4478108

--- src/com/android/services/telephony/TelecomAccountRegistry.java
+++ src/com/android/services/telephony/TelecomAccountRegistry.java
@@ -558,14 +558,6 @@ public class TelecomAccountRegistry {
                 capabilities |= PhoneAccount.CAPABILITY_CHANGE_RTT_CALL_TO_AUDIO_CALL;
             }
 
-// QTI_BEGIN: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
-            if (isRttDowngradeSupported()) {
-// QTI_END: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
-                capabilities |= PhoneAccount.CAPABILITY_DOWNGRADE_RTT;
-// QTI_BEGIN: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
-            }
-
-// QTI_END: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
             if (mIsCallComposerCapable) {
                 capabilities |= PhoneAccount.CAPABILITY_CALL_COMPOSER;
             }
@@ -1210,26 +1202,6 @@ public class TelecomAccountRegistry {
             }
         }
 
-// QTI_BEGIN: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
-        /**
-         * Determines whether RTT downgrade is supported given the current state of the
-         * device.
-         */
-        private boolean isRttDowngradeSupported() {
-            /**
-             * We can return the current cached value for both sim and simless case
-             * when the device has sim, cached value will have the current value
-             * for simless case, it will have the previous sub's config value, but in simless only
-             * emergency call is supported, it's assumend this API will be called for emergency
-             * RTT only.
-             */
-            int simLessRttDowngradeSupported = Settings.Secure.getInt(
-                    mContext.getContentResolver(), RTT_DOWNGRADE_SUPPORTED +
-                    convertRttPhoneId(mPhone.getPhoneId()), RTT_DOWNGRADE_NOT_SUPPORTED);
-            return simLessRttDowngradeSupported != RTT_DOWNGRADE_NOT_SUPPORTED;
-        }
-
-// QTI_END: 2023-01-18: Telephony: IMS : Move RTT downgrade and upgrade logic completely to AOSP.
         /**
          * Determines whether RTT is supported given the current state of the
          * device.
